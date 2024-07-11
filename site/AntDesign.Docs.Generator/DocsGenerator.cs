7812762e4ccef5e2da39e957c4018697a2871ea5
James Yeung <shunjiey@hotmail.com>
2024-07-11 18:19:27 +0800

    refactor source generator

---
+++ site/AntDesign.Docs.Generator/DocsGenerator.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis;
+
+namespace AntDesign.Docs.Generator
+{
+    [Generator]
+    public class Generator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext initContext)
+        {
+            // define the execution pipeline here via a series of transformations:
+
+            // find all additional files that end with .txt
+            IncrementalValuesProvider<AdditionalText> textFiles = initContext.AdditionalTextsProvider.Where(static file => file.Path.EndsWith(".md"));
+
+            // read their contents and save their name
+            IncrementalValuesProvider<(string name, string content)> namesAndContents = textFiles.Select((text, cancellationToken) => (name: Path.GetFileNameWithoutExtension(text.Path), content: text.GetText(cancellationToken)!.ToString()));
+
+            // generate a class that contains their values as const strings
+            initContext.RegisterSourceOutput(namesAndContents, (spc, nameAndContent) =>
+            {
+                spc.AddSource($"markdown.docs.{nameAndContent.name.Replace(".", "_")}.g.cs", $@"
+                    public partial class Docs_{nameAndContent.name.Replace(".", "_")}
+                    {{
+                        public const string markdown = ""{Markdig.Markdown.ToHtml(nameAndContent.content)}"";
+                    }}");
+            });
+        }
+    }
+}
